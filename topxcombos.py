c427619b5b2fa7c9a1bffcf56c2ae5f216ab9cbf
U-Michael-PC\Michael <Michael@Michael-PC.(none)>
2013-06-06 14:34:33 -0700

    updated README

--- topxcombos.py
+++ topxcombos.py
@@ -10,10 +10,41 @@
 #=============================================================================
 
 from argparse import ArgumentParser
-import math
+
+def total_lines(infile):
+    template = open(infile)
+    num_lines = sum(1 for line in template)
+    template.close()
+    
+    return num_lines
+    
+def lines_to_return(percent, total_lines):
+    decimal_val = 0.01*percent
+    lines_returned = int(decimal_val * total_lines)
+    return lines_returned
+    
+def write_output(infile, outfile, percent, lines_to_return, total_lines):
+    
+    newfile = open(outfile, "w")
+    
+    newfile.write("Top %d percent of combinations from %s\n" % (percent, infile))
+    newfile.write("%d of %d combinations selected\n" % (lines_to_return, total_lines))
+
+    template = open(infile)
+    
+    lineindex = 0    
+    for line in template:
+        if lineindex == lines_to_return:
+            break
+        newfile.write(line)
+        lineindex += 1
+        
+    template.close()
+    newfile.close()
 
 def main():
     
+    # read command-line arguments
     parser = ArgumentParser(description="Indicate top X percent of overhang combinations to retrieve")
     
     parser.add_argument("infile", help="Input file with results from GGSOFT")
@@ -26,31 +57,14 @@ def main():
     outfile = args.outfile
     percent = float(args.percent)
     
-    newfile = open(outfile, "w")
-    
     # find total number of lines in the input file
-    template = open(infile)
-    num_lines = sum(1 for line in template)
-    template.close()    
+    num_lines = total_lines(infile)
     
     # calculate number of lines to copy to new file based on percent specified
-    decimal_val = 0.01*percent
-    lines_returned = int(decimal_val * num_lines)
+    lines_returned = lines_to_return(percent, num_lines)
 
     # create output file with top X percent of combinations
-    newfile.write("Top %d percent of combinations from %s\n" % (percent, infile))
-    newfile.write("%d of %d combinations selected\n" % (lines_returned, num_lines))
-
-    template = open(infile)
-    lineindex = 0    
-    for line in template:
-        if lineindex == lines_returned:
-            break
-        newfile.write(line)
-        lineindex += 1
-        
-    template.close()
-    newfile.close()
+    write_output(infile, outfile, percent, lines_returned, num_lines)
 
 if __name__ == "__main__":
     main()
